a91e5d00f5ceb7781f1e72270fc90e457a1693da
Charles Martin Reed <charlesmartinreed@icloud.com>
2023-05-14 01:09:02 -0500

    Create Post class.

--- classes/Post.js
+++ classes/Post.js
@@ -1,8 +1,39 @@
-class Post {
+module.exports = class Post {
   constructor(postTitle, postBody) {
     this.postTitle = postTitle;
     this.postBody = postBody;
-    this.postId = "";
-    this.postTimestamp = "";
+    this.postId = this.getPostId();
+    this.postTimestamp = new Date();
   }
-}
+
+  #setPostId = (idLen = 16) => {
+    const randomizeRanges = [
+      () => {
+        return Math.floor(Math.random() * (122 - 97) + 97);
+      },
+      () => {
+        return Math.floor(Math.random() * (90 - 65) + 65);
+      },
+      () => {
+        return Math.floor(Math.random(57 - 48) + 48);
+      },
+    ];
+
+    let generatedIDValues = [];
+
+    for (let i = 0; i < idLen; i++) {
+      let m =
+        randomizeRanges[
+          Math.floor(Math.random() * (randomizeRanges.length - 0) + 0)
+        ]();
+
+      generatedIDValues = [...generatedIDValues, String.fromCharCode(m)];
+    }
+
+    return generatedIDValues.join("");
+  };
+
+  getPostId() {
+    return this.#setPostId();
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const Post = require("../classes/Post");
 
 const PORT = process.env.PORT || 7000;
 app.use(express.json());
