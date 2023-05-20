46681f1b8b76ca2f1b37d0c12ff290f841d421a6
Charles Martin Reed <charlesmartinreed@icloud.com>
2023-05-19 19:27:37 -0500

    Convert input, textarea to div.

--- pages/scripts/index.js
+++ pages/scripts/index.js
@@ -1,6 +1,6 @@
-const inputPostTitleEl = document.querySelector("#input__post__title");
-const textareaPostBodyEl = document.querySelector("#textarea__post");
-const formEl = document.querySelector("#form__new__edit__post");
+const divPostTitleEl = document.querySelector("#div__post__title");
+const divPostBodyEl = document.querySelector("#div__post__body");
+const btnSubmitEl = document.querySelector("#submit__post__button");
 const btnsRichTextEls = document.querySelectorAll(".btn__richtext__ui");
 
 let richTextButtonState = {
@@ -10,26 +10,18 @@ let richTextButtonState = {
   set_strike: { state: false, cssClass: "text-is-stricken" },
 };
 
-window.addEventListener("DOMContentLoaded", (e) => {
-  // let p = document.createElement("p");
-  // p.classList.add("text-is-bolded", "text-is-emphasized");
-  // p.textContent = "Bolded text here.";
-  // formEl.appendChild(p);
-  // console.log(textareaPostBodyEl.innerHTML);
-});
-
 let richTextActiveClasses = [];
 let testingURL = "http://localhost:7000/api";
 let textAreaBody = ``;
 
 let pressedKeys = [];
 
-formEl.addEventListener("submit", async (e) => {
-  e.preventDefault();
+btnSubmitEl.addEventListener("click", async (e) => {
+  // e.preventDefault();
   // TESTING STATE
   // NOT CHECKING FOR VALID DATA, ETC
-  let postTitle = inputPostTitleEl.value;
-  let postBody = textareaPostBodyEl.value;
+  let postTitle = divPostTitleEl.innerHTML;
+  let postBody = divPostBodyEl.innerHTML;
 
   if (postTitle !== "" && postBody !== "") {
     // let data = new FormData(formEl);
@@ -102,14 +94,14 @@ function openRichTextPortion(cssClass) {
 
   // richTextActiveClasses += `${cssClass} `;
 
-  // let spanNodes = Array.from(textareaPostBodyEl.children).filter((node) =>
+  // let spanNodes = Array.from(divPostBodyEl.children).filter((node) =>
   //   node.classList.contains("span__rich__text__container")
   // );
 
   // if (spanNodes.length === 0) {
   // }
 
-  // for (const children of textareaPostBodyEl) {
+  // for (const children of divPostBodyEl) {
   //   if (children.length === 0) {
   //     let openTag = document.querySelector(`#span__rich__text__container__0`)
   //   } else {
@@ -185,8 +177,8 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
-textareaPostBodyEl.addEventListener("focus", (e) => {
-  textareaPostBodyEl.addEventListener("keydown", (e) => {
+divPostBodyEl.addEventListener("focus", (e) => {
+  divPostBodyEl.addEventListener("keydown", (e) => {
     // if (e.code.includes("Key")) {
     //   // textareaPostBodyEl.textContent += e.key;
     //   // console.log("current textareabody html", textareaPostBodyEl.textContent);
